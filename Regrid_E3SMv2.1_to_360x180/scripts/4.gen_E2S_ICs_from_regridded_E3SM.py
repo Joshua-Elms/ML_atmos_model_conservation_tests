ff40e19ca94f47e4f5daf55d6d8e2cdfdfb241e3
Joshua Elms <joshua.elms111@gmail.com>
2026-01-13 13:07:06 -0500

    need to fix regrid process I guess

--- Regrid_E3SMv2.1_to_360x180/scripts/4.gen_E2S_ICs_from_regridded_E3SM.py
+++ Regrid_E3SMv2.1_to_360x180/scripts/4.gen_E2S_ICs_from_regridded_E3SM.py
@@ -11,10 +11,9 @@ e2s_to_e3sm = dict(
     u100m="UBOT",
     v100m="VBOT",
     t2m="TBOT",
-    sp="ps",
-    msl=None,
+    sp="PS",
+    msl="PSL",
     tcwv="TCWV",
-    tp06=None,
 )
 for lev in model_info.STANDARD_13_LEVELS:
     for var in ["u", "v", "t", "q", "r", "z"]:
@@ -23,7 +22,7 @@ for lev in model_info.STANDARD_13_LEVELS:
         )
 
 # choose unperturbed or perturbed run
-pert = False
+pert = True
 
 if pert:
     regridded_e3sm_path = Path(
@@ -34,7 +33,7 @@ else:
         "/N/scratch/jmelms/ML_atmos_model_conservation_tests_scratch_data/B.energy_balance/data/original_regridded"
     )
 
-regridded_e3sm_path = regridded_e3sm_path / "E3SM_original_regridded_IC.nc"
+regridded_e3sm_path = regridded_e3sm_path / "E3SM_IC.nc"
 
 # models to create ICs for
 models = ["SFNO", "Pangu24"]
@@ -50,19 +49,24 @@ full_ds = xr.open_dataset(regridded_e3sm_path)
 # add TCWV field
 
 # add relative humidity (R) field on levels
-breakpoint()
 for lev in model_info.STANDARD_13_LEVELS:
     p = int(lev) * metpy.units.units("hPa")  # convert hPa to Pa
     t_var = f"T{lev}"
     q_var = f"Q{lev}"
     if t_var in full_ds.data_vars and q_var in full_ds.data_vars:
         T = full_ds[t_var] * metpy.units.units("K")  # K
-        q = full_ds[q_var] * metpy.units.units("g/kg")  # %
+        q = full_ds[q_var] * metpy.units.units("g/g")  #
         # clip below 0. this will cause nan td, so we'll then replace those with 0 q.
-        q = q.clip(min=0.0 * metpy.units.units.percent)
-        td = metpy.calc.dewpoint_from_specific_humidity(T, q)
+        q = q.clip(min=0.0)
+        Td = metpy.calc.dewpoint_from_specific_humidity(
+            pressure=p, specific_humidity=q
+        ).metpy.convert_units("K")
+        Td = np.where(Td > T, T, Td) * metpy.units.units("K")
+        breakpoint()
         r = metpy.calc.relative_humidity_from_dewpoint(
-            p, td, phase="auto"
+            temperature=T, dewpoint=Td, phase="auto"
         ).metpy.magnitude
-        q = np.nan_to_num(q, nan=0.0)
-        tmp_ds[f"q{level}"] = (tmp_ds[rh_var].dims, q)
+        # r = np.nan_to_num(q, nan=0.0).clip(min=0.0, max=1.0)
+        full_ds[f"R{lev}"] = (full_ds[q_var].dims, r)
+
+breakpoint()
